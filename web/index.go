fb17998384d2e6880b78f63033b10333761167a2
lanthora <lanthora@outlook.com>
2024-05-21 08:59:01 +0000

    Show all devices

--- web/index.go
+++ web/index.go
@@ -14,6 +14,7 @@ func Index(c *gin.Context) {
 	online := int64(0)
 	daily := int64(0)
 	weekly := int64(0)
+	all := int64(0)
 	domain := int64(0)
 	user := int64(0)
 
@@ -22,12 +23,14 @@ func Index(c *gin.Context) {
 		storage.Model(&candy.Device{}).Where("online = true").Count(&online)
 		storage.Model(&candy.Device{}).Where("online = true").Or("conn_updated_at > ?", time.Now().AddDate(0, 0, -1)).Count(&daily)
 		storage.Model(&candy.Device{}).Where("online = true").Or("conn_updated_at > ?", time.Now().AddDate(0, 0, -7)).Count(&weekly)
+		storage.Model(&candy.Device{}).Count(&all)
 		storage.Model(&candy.Domain{}).Count(&domain)
 		storage.Model(&User{}).Count(&user)
 	} else {
 		storage.Model(&candy.Device{}).Where("online = true AND username = ?", currentUser.Name).Count(&online)
 		storage.Model(&candy.Device{}).Where("online = true AND username = ?", currentUser.Name).Or("conn_updated_at > ? AND username = ?", time.Now().AddDate(0, 0, -1), currentUser.Name).Count(&daily)
 		storage.Model(&candy.Device{}).Where("online = true AND username = ?", currentUser.Name).Or("conn_updated_at > ? AND username = ?", time.Now().AddDate(0, 0, -7), currentUser.Name).Count(&weekly)
+		storage.Model(&candy.Device{}).Where("username = ?", currentUser.Name).Count(&all)
 		storage.Model(&candy.Domain{}).Where("username = ?", currentUser.Name).Count(&domain)
 	}
 
@@ -35,6 +38,7 @@ func Index(c *gin.Context) {
 		"online": online,
 		"daily":  daily,
 		"weekly": weekly,
+		"all":    all,
 		"domain": domain,
 		"role":   currentUser.Role,
 		"user":   user,
